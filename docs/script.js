2de4c4bc50edd2dc00a004a7c6e55052807fbce8
KozakevichVitaliy <samcroc2012@gmail.com>
2018-11-25 15:48:54 +0200

    сщььше №1

---
+++ docs/script.js
@@ -0,0 +1,34 @@
+var menu = document.getElementById('menu');
+menu.onclick = function myFunction() {
+    var x = document.getElementById('myTopnav');
+   if (x.className === 'topnav') {
+       x.className += " responsive";
+   } else {
+       x.className = 'topnav';
+   } 
+}
+
+
+$(function() {
+ 
+    $(window).scroll(function() {
+     
+        if($(this).scrollTop() != 0) {
+     
+            $('#toTop').fadeIn();
+     
+        } else {
+     
+            $('#toTop').fadeOut();
+     
+        }
+     
+    });
+     
+    $('#toTop').click(function() {
+     
+        $('body,html').animate({scrollTop:0},800);
+     
+    });
+     
+});
